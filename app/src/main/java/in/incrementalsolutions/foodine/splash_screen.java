17b05770649849fbf76015d17ba7267733899e7b
mgupta11 <m.gupta11@gmail.com>
2015-02-15 09:54:03 +0530

    Added a static splash screen with 5 secs delay

--- app/src/main/java/in/incrementalsolutions/foodine/splash_screen.java
+++ app/src/main/java/in/incrementalsolutions/foodine/splash_screen.java
@@ -1,9 +1,12 @@
 package in.incrementalsolutions.foodine;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 
 public class splash_screen extends ActionBarActivity {
@@ -12,6 +15,15 @@ public class splash_screen extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Integer seconds_delayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splash_screen.this, restaurant_list.class));
+                finish();
+            }
+        }, seconds_delayed*5000);
     }
 
 
